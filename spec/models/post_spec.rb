5676ce149ee43dca437052aa3d4cb47aaaa49b1d
Josh Morris <jmo0624@gmail.com>
2020-05-07 23:58:55 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Post do
-  
+  it 'can be created' do
+    post = Post.create!(title: "my title", description: "The post description")
+    expect(post).to be_valid
 end
