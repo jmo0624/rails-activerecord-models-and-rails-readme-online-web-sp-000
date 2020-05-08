f4bc45f724e826faf193548ff53796bc61888065
Josh Morris <jmo0624@gmail.com>
2020-05-08 00:01:11 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < AvticeRecord::Migration
+  
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      
+      t.timestamps null: false
+      
+    end
+  end
+end
